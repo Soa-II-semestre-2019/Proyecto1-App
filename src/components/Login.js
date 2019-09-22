aded459add2a2ff12204e348b128c45e4d5284f3
AllanCh <edochacon96@gmail.com>
2019-09-22 10:18:10 -0600

    Cear pantalla de inicio de sesion
    Se crea la pantalla de inicio de sesion

---
+++ src/components/Login.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import {StyleSheet} from 'react-native';
+import { Container, Content, Card, CardItem, Text, Body, Button, Item, Input } from 'native-base'
+import { FontAwesome } from '@expo/vector-icons'
+import { black } from 'ansi-colors';
+
+class Login extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { loading: true };
+      }
+
+      async componentWillMount() {
+        await Expo.Font.loadAsync({  
+          Roboto_medium: require("../../node_modules/native-base/Fonts/Roboto_medium.ttf"),
+        });
+        this.setState({ loading: false });
+      }
+
+    render () {
+        if (this.state.loading) {
+            return <Text style={styles.loadingApp}>Cargando Aplicacion</Text>;
+        }
+        return (
+            <Container style={styles.container} >
+            <Content padder contentContainerStyle={styles.content}>
+              <Card>
+                <CardItem header bordered>
+                  <Text style={styles.textCenter}>Inicio de Sesión</Text>
+                </CardItem>
+                <CardItem bordered>
+                    <Body>
+                    <Item inlineLabel>
+                        <FontAwesome name='user-circle' size={20}></FontAwesome>
+                        <Input placeholder='Usuario'/>
+                    </Item>
+                    <Item inlineLabel last>
+                    <FontAwesome name='lock' size={20}></FontAwesome>
+                        <Input placeholder='Contraseña'/>
+                    </Item>
+                  </Body>
+                </CardItem>
+                <CardItem footer bordered> 
+                  <Button primary style={styles.button}>
+                      <Text>Entrar</Text> 
+                  </Button>
+                </CardItem>
+              </Card>
+            </Content>
+          </Container>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    textCenter: {
+        textAlign: 'center', 
+        width: '100%'
+    },
+    content: {
+        flex: 1,
+        justifyContent: 'center'
+    },
+    loadingApp: {
+        textAlign: 'center', 
+        width: '100%',
+        justifyContent: 'center'
+    },
+    button: {
+        marginLeft: '70%'
+    },
+    container: {
+        backgroundColor: 'white'
+    }
+})
+
+export default Login
